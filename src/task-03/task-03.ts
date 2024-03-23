86c896b369bd4ee048a2471525a36dd04c275b0f
Kukucska Johanna <johannakukucska14@gmail.com>
2024-03-23 21:43:13 +0100

    "task-01, task-02, task-03, task-05"

--- src/task-03/task-03.ts
+++ src/task-03/task-03.ts
@@ -1,5 +1,21 @@
 import { Draw, Game } from './models';
 
+
 export const minimalCubeSet = (games: Game[]): number => {
-  throw new Error('Not implemented');
+  let totalStrength = 0;
+
+  games.forEach(game => {
+    let minRed = 0, minGreen = 0, minBlue = 0;
+
+    game.draws.forEach(draw => {
+      if (draw.red && draw.red > minRed) minRed = draw.red;
+      if (draw.green && draw.green > minGreen) minGreen = draw.green;
+      if (draw.blue && draw.blue > minBlue) minBlue = draw.blue;
+    });
+
+    const gameStrength = minRed * minGreen * minBlue;
+    totalStrength += gameStrength;
+  });
+
+  return totalStrength;
 };
